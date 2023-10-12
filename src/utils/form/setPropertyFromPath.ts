3eb1586597b99cf7cb849ad8761489398f3c1e89
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-10-12 14:45:11 +0200

    feat: implement scope-based property getter/setter

--- src/utils/form/setPropertyFromPath.ts
+++ src/utils/form/setPropertyFromPath.ts
@@ -14,12 +14,33 @@ export function setPropertyFromPath(
     ...key.split(".").slice(1),
   ];
 
-  console.log({ offset, properties, value });
-
   properties.reduce(
     (o, p, i) =>
       (o[p as string] =
         properties.length === ++i ? value : o[p as string] || {}),
     object
   );
+}
+
+export function propertySetter(
+  target: string,
+  parentKey: (string | number)[],
+  state: Record<string, unknown>,
+  value: any
+) {
+  if (target === "$root") state = value;
+  else if (target.includes("$parent"))
+    setPropertyFromPath(state, [...(parentKey ?? [])], value, target);
+  else return setPropertyFromPath(state, target, value);
+}
+
+export function propertyResolver(
+  target: string,
+  parentKey: (string | number)[],
+  state: Record<string, unknown>
+) {
+  if (target === "$root") return state;
+  else if (target.includes("$parent"))
+    return getPropertyFromPath([...(parentKey ?? [])], state, target);
+  else return getPropertyFromPath(target, state);
 }
