d8bff8466096c0535c1c037868e5fad00e00cf37
Cyprien Thao <55083156+ChronicStone@users.noreply.github.com>
2023-10-15 18:05:13 +0200

    docs: base init & config

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,38 +1,46 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'Sweettools | High-quality DX & type-safety focused suite of Vue 3 UI components & utilities',
+    description: 'High-quality DX & type-safety focused suite of Vue 3 UI components & utilities. Forms, DataTables, Excel import, and more !',
+    image: '/cover.png',
+
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: '',
+      github: 'https://github.com/ChronicStone/vue-sweettools',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
       }
     },
+
     github: {
       dir: '.starters/default/content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      repo: 'Vue-Sweettools',
+      owner: 'ChronicStone',
+      edit: true,
+      baseUrl: 'https://github.com/ChronicStone/vue-sweettools'
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
       fluid: true
-    }
+    },
+
+    titleTemplate: '%s · Sweettools'
   }
 })
