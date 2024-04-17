fbfe63454909e1f564a74a81a8d811225ae6bbae
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2024-04-17 16:11:41 +0200

    feat(form): implement tag field drag re-ordering

--- src/form/components/Field/TagField.vue
+++ src/form/components/Field/TagField.vue
@@ -1,20 +1,84 @@
-<script setup lang="ts">
-import type { DynamicTagsOption } from 'naive-ui'
-import { NDynamicTags } from 'naive-ui'
+<script setup lang="tsx">
+import { NDynamicTags, NTag } from 'naive-ui'
+import { useDraggable } from 'vue-draggable-plus'
 import type { FieldComponentEmits, FieldComponentProps } from '@/form/types/fields'
 
 const props = defineProps<FieldComponentProps>()
 const emit = defineEmits<FieldComponentEmits>()
 const fieldValue = computed({
-  get: () => props.modelValue as (string | DynamicTagsOption)[] | undefined,
+  get: () => props.modelValue as string[] | undefined,
   set: value => emit('update:modelValue', value),
+})
+
+function removeTag(index: number) {
+  fieldValue.value?.splice(index, 1)
+}
+
+const draggable = computed(() => props.context.rawInputProps.value.draggable ?? false)
+const customTagRender = computed(() => props.context.inputProps.value.renderTag)
+const renderTag = computed(() => (value: string, index: number) => {
+  return (
+    <NTag closable onClose={() => removeTag(index)}>
+      <div class="flex items-center gap-1">
+        { draggable.value && <span class="iconify cursor-grab drag-handle" data-icon="material-symbols:drag-indicator" /> }
+        { typeof customTagRender.value === 'function' ? customTagRender.value(value, index) : value }
+      </div>
+    </NTag>
+  )
+})
+
+const dragStarted = ref<boolean>(false)
+const tagsRef = ref<InstanceType<typeof NDynamicTags>>()
+const tagsContainerRef = ref<HTMLElement>()
+const { start, pause, resume } = useDraggable(tagsContainerRef, ref([]), {
+  immediate: false,
+  animation: 150,
+  handle: '.drag-handle',
+  onEnd: (event) => {
+    const { oldIndex, newIndex } = event
+    if (oldIndex === undefined || newIndex === undefined)
+      return
+    const tags = fieldValue.value?.slice() ?? []
+    const [removed] = tags.splice(oldIndex, 1)
+    tags.splice(newIndex, 0, removed)
+    fieldValue.value = tags
+  },
+})
+
+watchEffect(() => {
+  tagsContainerRef.value = tagsRef.value?.$el
+})
+
+watchOnce(() => tagsContainerRef.value, (el) => {
+  if (el && draggable.value) {
+    start()
+    dragStarted.value = true
+  }
+})
+
+watch(() => draggable.value, (enable) => {
+  if (enable && !dragStarted.value) {
+    start()
+    dragStarted.value = true
+  }
+  else if (!enable && dragStarted.value) {
+    pause()
+    dragStarted.value = false
+  }
+  else if (enable && dragStarted.value) {
+    resume()
+  }
 })
 </script>
 
 <template>
   <NDynamicTags
+    ref="tagsRef"
     v-model:value="fieldValue"
-    v-bind="context.inputProps.value"
+    v-bind="{
+      ...props.context.inputProps.value,
+      renderTag,
+    }"
     :disabled="disabled"
     @blur="validator?.$touch"
   />
